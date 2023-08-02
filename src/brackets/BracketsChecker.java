a887c7b7c6cbf0b4cdb3339e759b0493805b8929
harutyunyanart <artur.harutyunyan908@gmail.com>
2023-08-02 12:50:17 +0400

    "second commit in utilities"

---
+++ src/brackets/BracketsChecker.java
@@ -0,0 +1,43 @@
+package brackets;
+
+import java.util.Stack;
+
+public class BracketsChecker {
+    public static void main(String[] args) {
+        String s = "sd9({f[f]d}[)fdkv";
+        System.out.println(isCorrect(s));
+    }
+
+    private static boolean isCorrect(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char e = s.charAt(i);
+            if (e == '(' || e == '{' || e == '[') {
+                stack.push(e);
+                continue;
+            }
+            switch (e) {
+                case ')':
+                    if (stack.empty() || stack.pop() != '(') {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (stack.empty() || stack.pop() != '{') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (stack.empty() || stack.pop() != '[') {
+                        return false;
+                    }
+                    break;
+            }
+
+        }
+        return stack.isEmpty();
+    }
+
+
+}
